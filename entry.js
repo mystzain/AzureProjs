f77fdb35a58352004e4f2be1466da01e2c7c844f
mystz <msytzain@live.cn>
2016-02-12 11:07:41 +0800

    Initial commit

---
+++ entry.js
@@ -0,0 +1,5 @@
+var ng = require('angular');
+require('./styles/site.styl');
+console.log(ng);
+document.write("abc");
+//# sourceMappingURL=entry.js.map
---
+++ entry.ts
@@ -0,0 +1,6 @@
+import * as ng from 'angular'
+import './styles/site.styl'
+
+console.log(ng);
+
+document.write("abc");
